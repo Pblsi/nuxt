b429b05ff781552bba90172f98cc657c114ccc71
paul blessing <paulblessing@gmail.com>
2023-09-10 21:55:47 +0100

    nuxt

--- pages/index.vue
+++ pages/index.vue
@@ -35,17 +35,17 @@
 
  -->
 
- <p class="transition-opacity duration-300 ease-in-out delay-100 hover:opacity-50">Hover me</p>
+ <!-- <p class="transition-opacity duration-300 ease-in-out delay-100 hover:opacity-50">Hover me</p> -->
 
     <!-- main nav -->
-    <nav class="fixed bg-ash-500 w-full md:h-20">
+    <nav class="fixed bg-black w-full font-medium py-2 md:h-20">
       <div class="text-white md:px-40">
         <div class="hidden md:flex mx-auto md:px-6 items-center">
           <!-- logo -->
 
           <div class="flex">
             <img src="~/assets/mti.png" class="w-8" alt="" />
-            <p class="text-2xl tracking-widest">MetaMask</p>
+            <p class="text-2xl tracking-widest">METAMASK</p>
           </div>
           <!-- primary nav -->
           <div class="flex mx-auto py-6 items-center space-x-10">
@@ -80,7 +80,7 @@
           <div class="justify-between px-6 py-4 flex items-center">
             <div class="flex items-center space-x-2">
               <img src="~/assets/mti.png" class="h-10" alt="" />
-              <p class="text-2xl text-white tracking-widest">MetaMask</p>
+              <p class="text-2xl text-white tracking-widest">METAMASK</p>
             </div>
             <div
               class="mobile-menu-button hover:cursor-pointer hover:bg-gray-800 hover:rounded-sm text-white"
@@ -148,21 +148,19 @@
       </div>
 
       <!-- hero 1 -->
-      <div class="bg-black pt-40 text-white">
+      <div class="bg-black p-8 pt-12 text-white">
         <div class="container mx-auto md:pl-40 md:flex items-center">
           <div class="">
             <h1
-              class="text-center md:text-left text-white text-2xl font-bold leading-normal tracking-tight"
-            >
-              Buy, store, send and swap tokens</h1
-            >
-            <p class="text-center md:text-left md:w-96">
+              class="text-center md:text-left text-white text-3xl font-bold leading-normal tracking-tight"
+            >Buy, store, send and swap tokens</h1>
+            <p class="pt-3 text-center md:text-left md:w-96">
               Available as a browser extension and as a mobile app, MetaMask
               equips you with a key vault, secure login, token wallet, and token
-              exchange—everything you need to manage your digital assets.
+              exchange- everything you need to manage your digital assets.
             </p>
           </div>
-          <img src="~/assets/pic3.png" class="max-w-md mx-auto" alt="" />
+          <img src="~/assets/pic3.png" class="max-w-md mx-auto pt-12" alt="" />
         </div>
       </div>
 
@@ -170,7 +168,7 @@
       <div class="lg:hidden mx-auto items-center px-4 md:flex">
         <div>
           <h1
-            class="text-left sm:text-center max-w-md text-3xl font-semibold leading-normal text-gray-100"
+            class="text-left max-w-md text-3xl font-semibold leading-normal text-gray-100"
           >
             Explore blockchain apps</h1
           >
@@ -189,7 +187,7 @@
         </div>
       </div>
       <!-- small screen -->
-      <div class="hidden md:flex md:mx-auto items-center px-4">
+      <div class="hidden sm:flex sm:mx-auto items-center p-8">
         <div class="">
           <img
             src="~/assets/Explore-illo.svg"
@@ -199,11 +197,11 @@
         </div>
         <div>
           <h1
-            class="text-left max-w-lg text-3xl font-semibold leading-normal text-gray-100"
+            class="sm:text-center max-w-lg sm:text-3xl sm:font-semibold leading-normal text-gray-100"
           >
             A crypto wallet & gateway to blockchain apps
           </h1>
-          <p class="text-left">
+          <p class="sm:text-center">
             Start exploring blockchain applications in seconds. Trusted by over
             30 million users worldwide.
           </p>
@@ -211,7 +209,7 @@
       </div>
 
       <!-- hero 3 -->
-      <div class="container mx-auto items-center px-4 md:flex">
+      <div class="container mx-auto items-center p-8 md:flex">
         <div>
           <h1
             class="text-left max-w-lg text-3xl font-semibold leading-normal text-gray-100"
@@ -230,35 +228,33 @@
         />
       </div>
 
-      <div class="p-10 bg-gray-800 text-center mx-auto items-center">
+      <div class="p-10 bg-gray-600 text-center mx-auto items-center">
         <img class="mx-auto w-60" src="~/assets/mt2.png" alt="" />
 
-        <p class="mt-20 mb-8 font-semibold text-2xl text-white">Get Started</p>
+        <p class="mt-20 mb-8 font-semibold text-3xl text-white">Get Started</p>
         <button
           type="button"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font- rounded-full text-sm px-5 py-4 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
         >
           Download Metamask Today
         </button>
       </div>
     </div>
 
-    <div class="w-full max-w-screen-lg mx-auto p-4">
+    <div class="w-full max-w-screen-lg mx-auto p-4 pb-12 bg-black">
       <div class="aspect-w-16 aspect-h-9">
-        <p class="text-center font-bold text-2xl p-4">What is MetaMask?</p>
+        <p class="text-center font-bold text-2xl pt-10 pb-10 text-white ">What is MetaMask?</p>
         <iframe
-          class="w-full h-full"
-          src="https://youtu.be/YVgfHZMFFFQ"
+          class="w-full h-80 rounded-md"
+          src="https://youtube.com"
           frameborder="0"
           allowfullscreen
         ></iframe>
       </div>
     </div>
 
-    <div class="border bg-gray-200">
-      <p class="font-bold text-center p-4 text-2xl"
-        >your key to blockchain applications</p
-      >
+    <div class="bg-gray-800 text-white py-10 px-6">
+      <p class="font-bold text-center p-4 text-3xl">Your key to blockchain applications</p>
       <p class="text-center"
         >MetaMask is the leading self-custodial wallet. The safe and simple way
         to access blockchain applications and web3. Trusted by millions of users
@@ -292,102 +288,86 @@
       </div>
     </div>
 
-    <div class="border bg-gray-800 p-10 text-center text-white">
-      <p class="">Developers</p>
-      <p
-        >MetaMask is powered by a strong community from across the globe.
-        Interested in contributing? Find out how and what to contribute using
-        the resources below.</p
-      >
-      <p>Open Issues></p>
-      <p>Open Bounties></p>
-      <p>Documentation></p>
-      <p>Contributing Guidelines></p>
+    <div class="bg-gray-600 p-10 text-center text-white">
+      <p class="text-4xl font-bold ">Developers</p>
+      <p class="pt-10 text-sm">MetaMask is powered by a strong community from across the globe.Interested in contributing? Find out how and what to contribute using the resources below.</p>
+      <p class="pt-10">Open Issues ></p>
+      <p class="pt-10" >Open Bounties ></p>
+      <p class="pt-10">Documentation ></p>
+      <p class="pt-10" >Contributing Guidelines ></p>
     </div>
 
-    <div class="border bg-gray-500 flex mx-auto items-center">
+    <div class="bg-gray-500 p-10">
+        <p class="font-bold text-white text-center text-2xl p-4">Shop MetaMask Swag</p>
       <div>
-        <img class="w-80" src="~/assets/pic1.jpg" alt="" />
-        <button class="text-center">Shop Now</button>
-        <p class="font-bold text-white text-center text-2xl p-4"
-          >Shop MetaMask Swag</p
-        >
+        <img class="" src="~/assets/pic1.jpg" alt="" />
+        <button class="border-2 rounded-full  p-3 px-10 border-white text-center text-white flex items-center mx-auto">Shop Now</button>
       </div>
     </div>
 
-    <div>
-      <p> Recieve our Newsletters </p>
-      <p> signup to recieve updates and announcements </p>
-    </div>
-    <button
-      type="button"
-      class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >Sign Up</button
-    >
+    <div class="bg-black text-white text-center p-20">
+      <p class="font-bold text-3xl"> Recieve our Newsletters </p>
+      <p class="pt-4"> signup to recieve updates and announcements </p>
+      <div class="pt-4">
+      <button class="p-3 px-4 rounded-full bg-blue-500 ">Sign Up</button>
+    </div></div>
 
     <!-- ////////////////////FOOTER -->
 
-    <footer class="bg-gray-950">
-      <div class="flex text-white">
+    <footer class="bg-gray-600 pt-12">
+        <div class="text-left text-white px-10 font-light text-sm">
+      <div class="flex text-white text-lg items-center">
         <img src="~/assets/mti.png" class="w-8" alt="" />
-        <p class="font-bold-sm tracking-widest">MetaMask</p>
+        <p class="font-semibold tracking-widest px-2">METAMASK</p>
       </div>
 
       <div
-        class="grid grid-cols-1 md:grid-cols-3 text-white text-center mx-auto gap-4"
+        class="grid grid-cols-1 md:grid-cols-3 pt-10"
       >
-        <div class="">
-          <p>LEARN MORE</p>
-          <ul class="">
+        <div class=" ">
+          <p class="text-xl orange ">LEARN MORE</p>
+          <ul class="pt-4">
             <li> About </li>
-            <li> Developers Docs </li>
-            <li> Download </li>
-            <li> Metamask Mask </li>
-            <li> Institutional News </li>
-            <li> Security </li>
+            <li class="pt-2"> Developers Docs </li>
+            <li class="pt-2"> Download </li>
+            <li class="pt-2"> MetamaskMask Institutional</li>
+            <li class="pt-2"> News </li>
+            <li class="pt-2"> Security </li>
           </ul>
         </div>
 
-        <div class="">
-          <p>GET INVOLVED</p>
-          <ul class="">
-            <li> GitHub </li>
-            <li> GitCoin </li>
-            <li> Open Positions </li>
-            <li> SwagShip </li>
-            <li> Press & Partnershops </li>
+        <div class="pt-6">
+          <p class="text-xl">GET INVOLVED</p>
+          <ul class="pt-4">
+            <li class="pt-2"> GitHub </li>
+            <li class="pt-2"> GitCoin </li>
+            <li class="pt-2"> Open Positions </li>
+            <li class="pt-2"> SwagShip </li>
+            <li class="pt-2"> Press & Partnershops </li>
           </ul>
         </div>
 
-        <div>
-          <p>LEGAL</p>
-          <ul>
-            <li>Privacy Policy</li>
-            <li>Terms of use</li>
-            <li>Contributor License Agreement</li>
-            <li>Site Map</li>
+        <div class="pt-6">
+          <p class="text-xl">LEGAL</p>
+          <ul class="pt-4">
+            <li class="pt-2">Privacy Policy</li>
+            <li class="pt-2">Terms of use</li>
+            <li class="pt-2">Contributor License Agreement</li>
+            <li class="pt-2">Site Map</li>
           </ul>
         </div>
       </div>
-      <div class="px-4 py-6 md:flex md:items-center md:justify-between">
-        <span class="text-sm text-gray-500 dark:text-gray-300 sm:text-center"
+      <div class="py-6 text-sm">
+        <span class="text-white"
           >© 2023 <a href="https://metamask.io/">MetaMask</a> • A Consensys
           Formation
         </span>
       </div>
+    </div>
     </footer>
   </div>
 </template>
 
 <script>
-// grab everything we need
-// const btn = document.querySelector("button.mobile-menu-button");
-// const menu = document.querySelector(".mobile-menu");
-
-// add event listeners
-// btn.addEventListener("click", (e) => {
-//     menu.classList.toggle('hidden');
-// });
-
 export default {};
 </script>
